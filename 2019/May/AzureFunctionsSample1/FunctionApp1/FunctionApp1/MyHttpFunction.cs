2a1e94858783841edb799a1b139d244f5c9a538d
Michael S. Collier <buckeye01@hotmail.com>
2019-05-12 22:09:30 -0400

    Changed welcome text.

--- 2019/May/AzureFunctionsSample1/FunctionApp1/FunctionApp1/MyHttpFunction.cs
+++ 2019/May/AzureFunctionsSample1/FunctionApp1/FunctionApp1/MyHttpFunction.cs
@@ -26,7 +26,7 @@ namespace FunctionApp1
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"You are AWESOME, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
