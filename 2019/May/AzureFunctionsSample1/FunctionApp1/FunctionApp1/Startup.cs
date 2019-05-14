bbbb790bf2d32d99beda677cb89b72aa62bbad00
Michael S. Collier <buckeye01@hotmail.com>
2019-05-13 20:14:53 -0400

    Final updates for May 2019 meetup. (#1)
    * Initial commit for simple Functions demo.
    
    * Changed welcome text.
    
    * Starting to add DI; added extension bundle.
    
    * Updates to deck.
    
    * Minor deck update.
    
    * Updated deck with info on Logic Apps and more Functions.
    
    * Include content from Vince and Mike.
    
    Final update before meeting.
    
    * Update ReadMe.md

---
+++ 2019/May/AzureFunctionsSample1/FunctionApp1/FunctionApp1/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Cosmos;
+
+[assembly: FunctionsStartup(typeof(FunctionApp1.Startup))]
+
+namespace FunctionApp1
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddScoped<IGreeter, AwesomeGreeter>();
+            builder.Services.AddSingleton((s) =>
+            {
+                return new CosmosClient(Environment.GetEnvironmentVariable("COSMOSDB_CONNECTIONSTRING"));
+            });
+        }
+    }
+}
---
+++ 2019/May/ReadMe.md
@@ -0,0 +1,1 @@
+See http://m.eado.ws/go/build19 for additional content.
